b5b88689e3e3893eaaefb3a34c0667cf260f79bc
Keeners Navarro <keeners.mejia@gmail.com>
2023-09-18 17:03:06 -0600

    Introduccion a Arreglos

---
+++ ArreglosUnidimensionales.java/src/App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        //Declaracion de arreglos
+        int arreglo1[] = new int[5];
+        int [] arreglo2 = new int[10];
+        int arreglo3[] = {1,2,3,4,5,6,7};
+        int []arreglo4 = new int [] {10, 20, 30, 40, 50};
+
+        System.out.println(arreglo1[2]);
+        System.out.println(arreglo2[7]);
+        System.out.println(arreglo3[5]);
+        System.out.println(arreglo4[4]);
+     }
+}
